b3270f66be29c6475b44df2ad711f268f17acd2f
Gerald Gonzalez <gedgonz7@gmail.com>
2016-04-04 15:00:55 -0600

    modify file

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,4 +1,4 @@
 var mongoose= require('mongoose');
-mongoose.connect('mongodb://ged:gedgonz791@ds015760.mlab.com:15760/blogjs');
-
+//mongoose.connect('mongodb://ged:gedgonz791@ds015760.mlab.com:15760/blogjs');
+mongoose.connect('mongodb://localhost/Blog');
 module.exports=mongoose;
