aed9779822d1d46083064cc835d47a2d9f6e943b
Gerald Gonzalez <gedgonz7@gmail.com>
2021-03-22 16:51:08 -0600

    add folder config

--- config/email.js
+++ config/email.js
@@ -5,63 +5,48 @@ const OAuth2 = google.auth.OAuth2;
 
 const createTransporter = async() => {
 
-        console.log(config.email);
-        console.log(config.passwordEmail);
+    console.log(config.email);
+    console.log(config.passwordEmail);
 
 
 
-        const oauth2Client = new OAuth2(
-            config.cliente_id,
-            config.cliente_secret,
-            "https://developers.google.com/oauthplayground"
-        );
+    const oauth2Client = new OAuth2(
+        config.cliente_id,
+        config.cliente_secret,
+        "https://developers.google.com/oauthplayground"
+    );
 
-        oauth2Client.setCredentials({
-            refresh_token: config.refresh_tocken
+    oauth2Client.setCredentials({
+        refresh_token: config.refresh_tocken
+    });
+
+    const accessToken = await new Promise((resolve, reject) => {
+        oauth2Client.getAccessToken((err, token) => {
+            if (err) {
+                reject();
+            }
+            console.log('tocke:' + token)
+            resolve(token);
         });
+    });
 
-        const accessToken = await new Promise((resolve, reject) => {
-            oauth2Client.getAccessToken((err, token) => {
-                if (err) {
-                    reject();
-                }
-                console.log('tocke:' + token)
-                resolve(token);
-            });
-        });
-
-        const smtpTransport = nodemailer.createTransport({
-            service: "gmail",
-            auth: {
-                type: "OAuth2",
-                user: config.email,
-                accessToken,
-                clientId: config.cliente_id,
-                clientSecret: config.cliente_secret,
-                refreshToken: config.refresh_tocken
-            },
-            tls: {
-                rejectUnauthorized: false
-            },
-        });
-
-        return smtpTransport;
-    }
-    /*
-    var smtpTransport = nodemailer.createTransport({
-        host: "smtp.gmail.com", // hostname
-        secure: true, // use SSL
-        port: 465, // port for secure SMTP
+    const smtpTransport = nodemailer.createTransport({
+        service: "gmail",
         auth: {
+            type: "OAuth2",
             user: config.email,
-            pass: config.passwordEmail
+            accessToken,
+            clientId: config.cliente_id,
+            clientSecret: config.cliente_secret,
+            refreshToken: config.refresh_tocken
         },
         tls: {
             rejectUnauthorized: false
         },
-        debug: true, // show debug output
-        logger: true // log information in console
     });
-    */
+
+    return smtpTransport;
+}
+
 
 module.exports.createTransporter = createTransporter;
